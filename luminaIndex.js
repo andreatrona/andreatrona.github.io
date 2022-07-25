c8846c3b2930b055e51c21e15db6201707e241e2
andrea.trona <andrea.trona1@gmail.com>
2022-07-25 02:18:05 +0200

    lumina player and test disappear

---
+++ luminaIndex.js
@@ -0,0 +1,24 @@
+/*const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick() {
+
+
+  btn.style.color = 'white';
+
+});
+
+
+*/
+
+function clientedetail() {
+  var el = document.getElementById("myButton");
+  el.classList.add("animating");
+
+  //This function runs when the CSS animation is completed
+/*  var listener = el.addEventListener('animationend', function() {
+    el.classList.remove("animating");
+
+    //this removes the listener after it runs so that it doesn't get re-added every time the button is clicked
+    el.removeEventListener('animationend', listener);
+  });*/
+}
