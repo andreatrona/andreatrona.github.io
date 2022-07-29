c0e5c7ce6a5f6efeca3f1e72438e4ccd632b376c
andrea.trona <andrea.trona1@gmail.com>
2022-07-29 14:33:18 +0200

    testo

--- luminaIndex.js
+++ luminaIndex.js
@@ -1,9 +1,8 @@
 function homePageFadeOut() {
   var el = document.getElementById("homePage");
   el.classList.add("animatingOut");
-  el = document.getElementById("homePage2.text");
+  el = document.getElementById("homePage2");
   el.classList.add("animatingIn");
-  el = document.getElementById("homePage2.nota");
-  el.classList.add("animatingIn");
+
 
 }
