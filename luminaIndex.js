dbda6900a0842188a3538dfd7fd10949075be56e
andrea.trona <andrea.trona1@gmail.com>
2022-07-29 14:32:21 +0200

    nota e background no anim

--- luminaIndex.js
+++ luminaIndex.js
@@ -1,7 +1,9 @@
 function homePageFadeOut() {
   var el = document.getElementById("homePage");
   el.classList.add("animatingOut");
-  el = document.getElementById("homePage2");
+  el = document.getElementById("homePage2.text");
+  el.classList.add("animatingIn");
+  el = document.getElementById("homePage2.nota");
   el.classList.add("animatingIn");
 
 }
