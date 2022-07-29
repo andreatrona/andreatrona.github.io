170aa6f36f82f56accd7354c0f92acecd4652254
andrea.trona <andrea.trona1@gmail.com>
2022-07-29 14:42:11 +0200

    add nota to animating in

--- luminaIndex.js
+++ luminaIndex.js
@@ -1,8 +1,10 @@
 function homePageFadeOut() {
   var el = document.getElementById("homePage");
   el.classList.add("animatingOut");
-  el = document.getElementById("homePage2");
+  el = document.getElementById("homePage2Testo");
   el.classList.add("animatingIn");
+  var nl = document.getElementById("homePage2Nota");
+  nl.classList.add("animatingIn");
 
 
 }
