5de463ad988458533949f93776b5851836ed4c12
andrea.trona <andrea.trona1@gmail.com>
2022-07-29 17:23:23 +0200

    gif

--- luminaIndex.js
+++ luminaIndex.js
@@ -15,3 +15,20 @@ function backgroundToWhite() {
   var el = document.getElementById('sfondo');
   el.classList.add("animWhite");
 }
+
+function backgroundToBlack() {
+  var el = document.getElementById('sfondo');
+  el.classList.remove("animWhite");
+  el.classList.add("animFlashBlack");
+}
+
+function backgroundToBlackRST() {
+  var el = document.getElementById('sfondo');
+  el.classList.add("black");
+  el.classList.remove("animFlashBlack");
+}
+
+function viewGIF(){
+  var el = document.getElementById('gifPlay');
+  el.classList.add("gifViewer");
+}
