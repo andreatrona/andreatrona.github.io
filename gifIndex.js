9116693e7cbee597c4a5066874f27a275cd0b1e2
andrea.trona <andrea.trona1@gmail.com>
2022-08-01 12:02:12 +0200

    gif Index detect ios

--- gifIndex.js
+++ gifIndex.js
@@ -17,3 +17,21 @@ function removeFlash2(){
   var el = document.getElementById('myLightDiv');
   el.classList.remove("gifFlasher2");
 }
+
+
+const standalone = window.navigator.standalone;
+const userAgent = window.navigator.userAgent.toLowerCase();
+const safari = /safari/.test(userAgent);
+const ios = /iphone|ipod|ipad/.test(userAgent);
+
+if (ios) {
+  if (!standalone && safari) {
+    console.log("browser");
+  } else if (standalone && !safari) {
+    console.log("standalone");
+  } else if (!standalone && !safari) {
+    console.log("uiwebview");
+  }
+} else {
+  console.log("no ios");
+}
